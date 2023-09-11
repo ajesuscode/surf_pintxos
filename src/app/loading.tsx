168aa42ca2bd99612b7a59583608ce29e7822790
aJesusCode <ajesusnode@gmail.com>
2023-09-11 16:06:48 +0200

    action test2

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Loading() {
+    //TODO fix custom loading
     return (
         <main className=" font-display text-5xl text-light flex flex-col items-center justify-center pt-48">
             loading ...
